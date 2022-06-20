9b54e6ebd60226833ee4b4277011772248e3098a
ScottDrysdale86 <sdrysdale86@pm.me>
2022-06-20 15:55:12 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,20 @@
+number_1 = 3
+number_2 = 5
+string_1 = 'Hello'
+string_2 = 'Everyone'
+
+def addition(number_1, number_2):
+    sum = number_1 + number_2
+    print(sum)
+
+def multiply(number_1, number_2):
+    total = number_1 * number_2
+    print(total)
+
+def combine(string_1, string_2):
+    sentance = string_1 + " " + string_2
+    print(sentance)
+
+addition(number_1, number_2)
+multiply(number_1, number_2)
+combine(string_1, string_2)
